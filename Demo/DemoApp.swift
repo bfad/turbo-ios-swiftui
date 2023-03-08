261ce55b172492be0cafd218bfb2c0e7fddacece
Brad Lindsay <sluggy.fan@gmail.com>
2023-03-08 15:22:34 -0600

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Brad Lindsay on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
